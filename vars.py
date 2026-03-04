4634785b8a8fc1882a2010b63a953505b4c1b02b
bugalia2007-cell <bugalia2007@gmail.com>
2026-03-04 22:34:37 +0530

    Refactor vars.py for improved environment handling
    Updated OWNER default value and added new environment variables for API and photo URLs.

--- vars.py
+++ vars.py
@@ -1,5 +1,3 @@
-#🇳‌🇮‌🇰‌🇭‌🇮‌🇱‌
-# Add your details here and then deploy by clicking on HEROKU Deploy button
 import os
 from os import environ
 
@@ -7,19 +5,24 @@ API_ID = int(environ.get("API_ID", "123"))
 API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-OWNER = int(environ.get("OWNER", ""))
+OWNER = int(environ.get("OWNER", "0"))
 CREDIT = environ.get("CREDIT", "Crystal")
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
 TOTAL_USER = os.environ.get('TOTAL_USERS', '').split(',')
-TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
+TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER if user_id.strip().isdigit()]
 
 AUTH_USER = os.environ.get('AUTH_USERS', '').split(',')
-AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
-if int(OWNER) not in AUTH_USERS:
+AUTH_USERS = [int(user_id) for user_id in AUTH_USER if user_id.strip().isdigit()]
+if OWNER != 0 and OWNER not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
-  
-# .....,.....,.......,...,.......,....., .....,.....,.......,...,.......,.....,
 
+api_url = environ.get("API_URL", "")
+api_token = environ.get("API_TOKEN", "")
+token_cp = environ.get("TOKEN_CP", "")
+adda_token = environ.get("ADDA_TOKEN", "")
 
-
+photologo = environ.get("PHOTO_LOGO", "https://envs.sh/GVI.jpg")
+photoyt = environ.get("PHOTO_YT", "https://envs.sh/GVI.jpg")
+photocp = environ.get("PHOTO_CP", "https://envs.sh/GVI.jpg")
+photozip = environ.get("PHOTO_ZIP", "https://envs.sh/GVI.jpg")
